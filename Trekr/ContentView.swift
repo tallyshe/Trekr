d00b2416d329fae033d69bf296f4f61a65fac28e
karhoshe <karhoshe@me.com>
2020-12-30 01:19:49 +0800

    Trekr App with SwiftUI
    Import json source file, images to asset
    updated contentView with VStack image and text

--- Trekr/ContentView.swift
+++ Trekr/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Image("highlands")
+                .resizable()
+                .scaledToFit()
+            
+            Text("The Highlands")
+        }
     }
 }
 
