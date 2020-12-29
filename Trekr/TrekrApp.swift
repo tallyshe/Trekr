b2818f0329fd43faf8948755137f81a6a5a1c9a6
karhoshe <karhoshe@me.com>
2020-12-30 00:14:36 +0800

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Kar Ho She on 30/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
